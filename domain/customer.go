33c455e4e3e9e5cdaf3e3550d35a1c4e33795c86
yaasin-raki2 <yassin23raki@gmail.com>
2021-07-27 11:20:45 +0100

    Migrating from database/sql stdlib to sqlx

--- domain/customer.go
+++ domain/customer.go
@@ -3,11 +3,11 @@ package domain
 import "github.com/yaasin-raki2/banking/errs"
 
 type Customer struct {
-	Id          string
+	Id          string `db:"customer_id"`
 	Name        string
 	City        string
 	ZipCode     string
-	DateofBirth string
+	DateofBirth string `db:"date_of_birth"`
 	Status      string
 }
 
--- domain/customerRepositoryDB.go
+++ domain/customerRepositoryDB.go
@@ -5,43 +5,29 @@ import (
 	"log"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/yaasin-raki2/banking/errs"
 	"github.com/yaasin-raki2/banking/logger"
 )
 
 type CustomerRepositoryDB struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDB) FindAll(status string) ([]Customer, *errs.AppError) {
-	var rows *sql.Rows
+	var customers []Customer
 	var err error
 
 	if status == "" {
-		rows, err = d.client.Query("SELECT * FROM customers")
+		err = d.client.Select(&customers, "SELECT * FROM customers")
 	} else {
-		rows, err = d.client.Query("SELECT * FROM customers WHERE status = $1", status)
+		err = d.client.Select(&customers, "SELECT * FROM customers WHERE status = $1", status)
 	}
 
 	if err != nil {
 		logger.Error("Error while querying cutomers table " + err.Error())
 		return nil, errs.NewUnexpectedError("unexpected database error")
-	}
-
-	var customers []Customer
-
-	for rows.Next() {
-		var c Customer
-
-		err := rows.Scan(&c.Id, &c.Name, &c.DateofBirth, &c.City, &c.ZipCode, &c.Status)
-
-		if err != nil {
-			logger.Error("Error while scanning the cutomers " + err.Error())
-			return nil, errs.NewUnexpectedError("unexpected database error")
-		}
-
-		customers = append(customers, c)
 	}
 
 	return customers, nil
@@ -50,11 +36,9 @@ func (d CustomerRepositoryDB) FindAll(status string) ([]Customer, *errs.AppError
 func (d CustomerRepositoryDB) ById(id string) (*Customer, *errs.AppError) {
 	customerSql := "SELECT * FROM customers WHERE customer_id = $1"
 
-	row := d.client.QueryRow(customerSql, id)
-
 	var c Customer
 
-	err := row.Scan(&c.Id, &c.Name, &c.DateofBirth, &c.City, &c.ZipCode, &c.Status)
+	err := d.client.Get(customerSql, id)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -71,7 +55,7 @@ func (d CustomerRepositoryDB) ById(id string) (*Customer, *errs.AppError) {
 func NewCustomerRepositoryDB() CustomerRepositoryDB {
 	connStr := "postgres://postgres:postgres@localhost:4000/postgres?sslmode=disable"
 
-	client, err := sql.Open("postgres", connStr)
+	client, err := sqlx.Open("postgres", connStr)
 
 	if err != nil {
 		log.Fatal(err)
